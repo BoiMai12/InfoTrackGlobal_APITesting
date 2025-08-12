be99fd4af98246c24d6bb216ed402877d8f7517b
BoiMai12 <mainukimboi+1@gmail.com>
2025-08-12 21:03:03 +0700

    adding classes to support read input data and hold actual result

---
+++ Cb/Libraries/StepDefinitions/GlobalSteps/Http/Model/HttpQueryParameterStepModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libraries.StepDefinitions.GlobalSteps.Http.Model;
+
+public class HttpQueryParameterStepModel
+{
+    public HttpQueryParameterStepModel(string name, string value)
+    {
+        Name = name;
+        Value = value;
+    }
+    public string Name { get; set; }
+    public string Value { get; set; }
+
+}
---
+++ Cb/Libraries/StepDefinitions/GlobalSteps/Http/Model/HttpRequestHeaderStepModel.cs
@@ -0,0 +1,18 @@
+﻿using Libraries.Support.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libraries.StepDefinitions.GlobalSteps.Http.Model;
+
+public class HttpRequestHeaderStepModel
+{
+    public List<HttpQueryParameter>? QueryParameters { get; set; }
+    public List<HttpRequestHeader>? QueryHeader { get; set; }
+    public string? Content { get; set; }
+    public string? ContentType { get; set; }
+
+
+}
